e25a00bc750082d9827c8f1c7234b88fa39c712f
sv-newmac <ssunnyvvats@outlook.com>
2025-06-13 19:51:21 +0530

    Revert "Merge pull request #2"
    Revert changes from main branch.
    Main branch should remain clean until end.
    This reverts commit 8422f9a12ff38de69420f0a3bf95f1a1c4b87c68, reversing
    changes made to 2fa38c881db6d26733570e02ed16f68e4982da78.

--- src/main/java/dev/sunny/core/security/CoreSecurityConfig.java
+++ src/main/java/dev/sunny/core/security/CoreSecurityConfig.java
@@ -4,9 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
@@ -17,15 +20,28 @@ public class CoreSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((requests) ->
-                        requests.requestMatchers("/myAccount", "/myBalance", "/loans", "/myCards", "/register")
-                                .authenticated()
-                                .requestMatchers("/notices", "/contact", "/error")
-                                .permitAll());
+        http.authorizeHttpRequests((requests) ->
+                requests.requestMatchers("/myAccount", "/myBalance", "/loans", "/myCards")
+                        .authenticated()
+                        .requestMatchers("/notices", "/contact", "/error")
+                        .permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService inMemoryUserDetails() {
+        UserDetails user = User.withUsername("user")
+                .password("{bcrypt}$2a$10$nCaHC8gGKld9TwrN.v8Gi.hZ4pyFZWbQn8DcndKuZ1nRpKvlsIWuO")
+                .authorities("read")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{bcrypt}$2a$10$2zytWFQ6sVGy0T6D/Z57LOFMY216JwqJA9BWvehSK.RmDczS1Aktm")
+                .authorities("read", "write")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
     @Bean
